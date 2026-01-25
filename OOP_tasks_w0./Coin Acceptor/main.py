e7c83102dc6cf6a56cbb5aa6193b738809da702f
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2026-01-26 01:57:33 +0200

    Update main.py

--- OOP_tasks_w0./Coin Acceptor/main.py
+++ OOP_tasks_w0./Coin Acceptor/main.py
@@ -14,26 +14,25 @@ def main():
     print("Program starting.")
 
     while True:
+      try:  
         show_menu()
         choice = input("Your choice: ")
-
         if choice == "1":
             coin_acceptor.insertCoin()
-
         elif choice == "2":
             amount = coin_acceptor.getAmount()
             print(f"Currently '{amount}' coins in coin acceptor")
-
         elif choice == "3":
             returned = coin_acceptor.returnCoins()
             print(f"Coin acceptor returned '{returned}' coins.")
-
         elif choice == "0":
             print("Program ending.")
             break
-
         else:
             print("Invalid choice. Please try again.")
+      except ValueError:
+          print("Please enter a valid integer choice.\n")
+      return None
 
 
 if __name__ == "__main__":
