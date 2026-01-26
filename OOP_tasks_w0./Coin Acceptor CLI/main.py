d3128a54adb7cb8e56035e15d5e7a7fa62ef3e66
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2026-01-26 02:00:07 +0200

    Improve coin insertion feedback messages
    Refactor print statements for clarity and formatting.

--- OOP_tasks_w0./Coin Acceptor CLI/main.py
+++ OOP_tasks_w0./Coin Acceptor CLI/main.py
@@ -5,19 +5,16 @@ from coin_acceptor2 import CoinAcceptor
 def main():
     print("Program starting.")
     print("Welcome to coin acceptor program.")
-
     coin_acceptor2 = CoinAcceptor()
-
     print("Insert new coin by typing it's value (0 returns the money, -1 exits the program)")
 
     while True:
         try:
             coin = float(input("Insert coin(0 return, -1 exit): "))
-
             if coin == -1:
                 print("Exiting program.")
                 break
-
+                
             elif coin == 0:
                 print("Returning coins...")
                 amount, value = coin_acceptor.returnCoins()
@@ -27,10 +24,8 @@ def main():
             else:
                 print("Inserting...")
                 coin_acceptor.insertCoin(coin)
-                print(
-                    f"Inserted coins = {coin_acceptor.getAmount()}, "
-                    f"value = {coin_acceptor.getValue()}€"
-                )
+                print(f"Inserted coins = {coin_acceptor.getAmount()}, "
+                print(f"value = {coin_acceptor.getValue()}€" )
 
         except ValueError:
             print("Please enter a valid coin value.")
