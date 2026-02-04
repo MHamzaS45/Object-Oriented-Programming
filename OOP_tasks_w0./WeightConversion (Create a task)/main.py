de3a6f8c56303b1d9fd032f52d675fb6db7d3564
MHamzaS45 <Hamza.Sahqani@student.lab.fi>
2026-02-04 13:10:52 +0200

    Update main.py

--- OOP_tasks_w0./WeightConversion (Create a task)/main.py
+++ OOP_tasks_w0./WeightConversion (Create a task)/main.py
@@ -19,7 +19,17 @@ def main():
      try:
        choice = int(input("Enter your choice: "))
        if choice == 1:
-         Feed = 
-
+         Feed = weight_conversion.setweight()
+       elif choice == 2:
+         Feed = weight_conversion.toKiloGram()
+       elif choice == 3:
+         Feed = weight_conversion.toMilliGram()
+       elif choice == 4:
+          Feed = weight_conversion.toTons()
+       elif choice == "0":
+            print("Program ending.")
+            break
+        else:
+            print("Invalid choice. Please try again.")
      except ValueError:
        print("Invalid output. Please re-enter with a valid integer option")
